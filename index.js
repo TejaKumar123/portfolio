ac008b11397f006484160682ab0715a9f4e1541e
Teja Kumar <97971538+TejaKumar123@users.noreply.github.com>
2024-02-14 23:11:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,22 +12,29 @@ window.addEventListener("load",()=>{
 	}
 })
 
+window.addEventListener("scroll",scrol_up);
+
+let sclinks=document.getElementById("scrol_links").children
+for(let i=0;i<sclinks.length;i++){
+	sclinks[i].setAttribute("onclick",'open_menu(0),scrol_to("'+sclinks[i].innerText.toLowerCase()+'")')
+}
+
 
 function mode_change(){
-	let ele=document.getElementById("mode");
+	let ele=document.getElementsByClassName("mode");
 	let rot=document.querySelector(":root");
 	let theme=localStorage.getItem("portfolio123theme");
 	if(theme=="dark"){
-		ele.style.left="-2px";
-		ele.children[0].innerHTML="light_mode";
+		ele[0].style.left=ele[1].style.left="-2px";
+		ele[0].children[0].innerHTML=ele[1].children[0].innerHTML="light_mode";
 		rot.style.setProperty("--bgcolor","white");
 		rot.style.setProperty("--txtcolor","black");
 		rot.style.setProperty("--modecolor","yellow");
 		localStorage.setItem("portfolio123theme","light");
 	}
 	else{
-		ele.style.left="20px";
-		ele.children[0].innerHTML="dark_mode";
+		ele[0].style.left=ele[1].style.left="20px";
+		ele[1].children[0].innerHTML=ele[1].children[0].innerHTML="dark_mode";
 		rot.style.setProperty("--bgcolor","black");
 		rot.style.setProperty("--txtcolor","rgba(255,255,255,0.9)");
 		rot.style.setProperty("--modecolor","black");
@@ -37,7 +44,7 @@ function mode_change(){
 
 function scrol_to(id){
 	let y=document.getElementById(id).offsetTop;
-	window.scrollTo(0,y);
+	window.scrollTo(0,y-50);
 }
 
 function scrol_up(){
@@ -46,6 +53,16 @@ function scrol_up(){
 	}
 	else{
 		document.getElementById("upside").style.display="none";
+	}
+}
+
+function open_menu(num){
+	let ele=document.getElementById("left_bar");
+	if(num){
+		ele.style.right="0px";
+	}
+	else{
+		ele.style.right="-300px";
 	}
 }
 
